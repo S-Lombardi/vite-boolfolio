888116aa5a25f5019e50a006568fa9624ca0b81c
S-Lombardi <silo.lombardi@gmail.com>
2023-08-29 19:23:57 +0200

    Installati i pacchetti necessari al progetto

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+  import AppMain from './components/AppMain.vue';
+
+  export default{
+    components: {
+      AppMain,
+    }
+  }
 </script>
 
 <template>
-  Holaaa
+  <div>
+    <AppMain/>
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+  @use './style/generals.scss'
 </style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,32 @@
+<script>  
+  export default{
+    name: 'AppMain',
+    data(){
+      return{
+
+      }
+    },
+    created(){
+
+    },
+    method (){
+
+    },
+  }
+</script>
+
+<template>
+  <div>
+    <div class="container">
+      <div class="row">
+        Hola
+      </div>
+    </div>
+
+  </div>
+  
+</template>
+
+<style scoped>
+
+</style>
