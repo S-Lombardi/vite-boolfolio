a6d81694399acb5e25ab38858b430659bdc47f2e
S-Lombardi <silo.lombardi@gmail.com>
2023-08-31 15:30:47 +0200

    Aggiunta Componente per il loading

---
+++ src/components/AppLoader.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+    
+}
+</script>
+
+<template lang="">
+    
+    <div class="spinner-border" role="status">
+        <span class="sr-only">
+            Loading...
+        </span>
+    </div>          
+        
+</template>
+
+<style lang="scss">
+
+@keyframes spinner-border {
+  to { transform: rotate(360deg) #{"/* rtl:ignore */"}; }
+}
+
+</style>
